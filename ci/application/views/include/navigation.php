4a25a359c06c4879de8c47190610c955ce7347b3
nandafirmans <nandafirmans@gmail.com>
2016-08-28 06:33:57 +0700

    update

---
+++ ci/application/views/include/navigation.php
@@ -0,0 +1,31 @@
+<header class="mn-header navbar-fixed">
+    <nav class="cyan darken-1">
+        <div class="nav-wrapper row">
+            <section class="material-design-hamburger navigation-toggle">
+                <a href="javascript:void(0)" data-activates="slide-out" class="button-collapse show-on-large material-design-hamburger__icon">
+                    <span class="material-design-hamburger__layer"></span>
+                </a>
+            </section>
+            <div class="header-title col s3 m3">      
+                <span class="chapter-title">Alpha</span>
+            </div>
+        </div>
+    </nav>
+</header>
+<aside id="slide-out" class="side-nav white fixed">
+    <div class="side-nav-wrapper">
+        <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+            <li class="no-padding active">
+                <a class="waves-effect waves-grey active" href="index-2.html"><i class="material-icons">settings_input_svideo</i>Dashboard</a>
+            </li>
+            <li class="no-padding">
+                <a class="waves-effect waves-grey" href="item"><i class="material-icons">apps</i>Items</a>
+            </li>
+           
+        </ul>
+        <div class="footer">
+            <p class="copyright">Steelcoders ©</p>
+            <a href="#!">Privacy</a> &amp; <a href="#!">Terms</a>
+        </div>
+    </div>
+</aside>
